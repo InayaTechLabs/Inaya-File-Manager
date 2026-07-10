fe15445e4e8f8429503363de2174ccd22778b758
InayaTechLabs <InayaTechLabs@gmail.com>
2026-07-10 10:52:36 +0700

    Add files via upload

--- admin/class-wpfm-admin.php
+++ admin/class-wpfm-admin.php
@@ -2,7 +2,7 @@
 /**
  * Admin side: menus, assets, and page rendering.
  *
- * @package Inaya_File_Manager
+ * @package WP_File_Manager
  */
 
 if ( ! defined( 'WPINC' ) ) {
@@ -11,7 +11,7 @@ if ( ! defined( 'WPINC' ) ) {
 
 class WPFM_Admin {
 
-	const MENU_SLUG = 'inaya-file-manager';
+	const MENU_SLUG = 'wp-file-manager';
 	const CAPABILITY = 'manage_options';
 
 	public function register_hooks() {
@@ -22,8 +22,8 @@ class WPFM_Admin {
 
 	public function add_menu() {
 		add_menu_page(
-			__( 'File Manager', 'inaya-file-manager' ),
-			__( 'File Manager', 'inaya-file-manager' ),
+			__( 'File Manager', 'wp-file-manager' ),
+			__( 'File Manager', 'wp-file-manager' ),
 			self::CAPABILITY,
 			self::MENU_SLUG,
 			array( $this, 'render_page' ),
@@ -34,7 +34,7 @@ class WPFM_Admin {
 
 	public function action_links( $links ) {
 		$url  = admin_url( 'admin.php?page=' . self::MENU_SLUG );
-		$open = '<a href="' . esc_url( $url ) . '">' . esc_html__( 'Open', 'inaya-file-manager' ) . '</a>';
+		$open = '<a href="' . esc_url( $url ) . '">' . esc_html__( 'Open', 'wp-file-manager' ) . '</a>';
 		array_unshift( $links, $open );
 		return $links;
 	}
@@ -54,12 +54,10 @@ class WPFM_Admin {
 		wp_enqueue_script(
 			'wpfm-admin',
 			WPFM_PLUGIN_URL . 'admin/js/wpfm-admin.js',
-			array( 'jquery', 'wp-codemirror' ),
+			array( 'jquery' ),
 			WPFM_VERSION,
 			true
 		);
-
-		wp_enqueue_code_editor( array( 'type' => 'text/plain' ) );
 
 		wp_localize_script(
 			'wpfm-admin',
@@ -68,27 +66,27 @@ class WPFM_Admin {
 				'ajaxUrl' => admin_url( 'admin-ajax.php' ),
 				'nonce'   => wp_create_nonce( 'wpfm_nonce' ),
 				'i18n'    => array(
-					'confirmDelete' => __( 'Are you sure you want to delete \"%s\"? This cannot be undone.', 'inaya-file-manager' ),
-					'promptFolder'  => __( 'New folder name:', 'inaya-file-manager' ),
-					'promptRename'  => __( 'New name:', 'inaya-file-manager' ),
-					'promptFile'    => __( 'New file name:', 'inaya-file-manager' ),
-					'empty'         => __( 'This folder is empty.', 'inaya-file-manager' ),
-					'loading'       => __( 'Loading…', 'inaya-file-manager' ),
-					'error'         => __( 'Error', 'inaya-file-manager' ),
-					'saved'         => __( 'Saved.', 'inaya-file-manager' ),
-					'uploading'     => __( 'Uploading…', 'inaya-file-manager' ),
-					'name'          => __( 'Name', 'inaya-file-manager' ),
-					'size'          => __( 'Size', 'inaya-file-manager' ),
-					'modified'      => __( 'Modified', 'inaya-file-manager' ),
-					'perms'         => __( 'Perms', 'inaya-file-manager' ),
-					'actions'       => __( 'Actions', 'inaya-file-manager' ),
-					'download'      => __( 'Download', 'inaya-file-manager' ),
-					'edit'          => __( 'Edit', 'inaya-file-manager' ),
-					'rename'        => __( 'Rename', 'inaya-file-manager' ),
-					'delete'        => __( 'Delete', 'inaya-file-manager' ),
-					'cancel'        => __( 'Cancel', 'inaya-file-manager' ),
-					'save'          => __( 'Save', 'inaya-file-manager' ),
-					'editing'       => __( 'Editing: %s', 'inaya-file-manager' ),
+					'confirmDelete' => __( 'Are you sure you want to delete "%s"? This cannot be undone.', 'wp-file-manager' ),
+					'promptFolder'  => __( 'New folder name:', 'wp-file-manager' ),
+					'promptRename'  => __( 'New name:', 'wp-file-manager' ),
+					'promptFile'    => __( 'New file name:', 'wp-file-manager' ),
+					'empty'         => __( 'This folder is empty.', 'wp-file-manager' ),
+					'loading'       => __( 'Loading…', 'wp-file-manager' ),
+					'error'         => __( 'Error', 'wp-file-manager' ),
+					'saved'         => __( 'Saved.', 'wp-file-manager' ),
+					'uploading'     => __( 'Uploading…', 'wp-file-manager' ),
+					'name'          => __( 'Name', 'wp-file-manager' ),
+					'size'          => __( 'Size', 'wp-file-manager' ),
+					'modified'      => __( 'Modified', 'wp-file-manager' ),
+					'perms'         => __( 'Perms', 'wp-file-manager' ),
+					'actions'       => __( 'Actions', 'wp-file-manager' ),
+					'download'      => __( 'Download', 'wp-file-manager' ),
+					'edit'          => __( 'Edit', 'wp-file-manager' ),
+					'rename'        => __( 'Rename', 'wp-file-manager' ),
+					'delete'        => __( 'Delete', 'wp-file-manager' ),
+					'cancel'        => __( 'Cancel', 'wp-file-manager' ),
+					'save'          => __( 'Save', 'wp-file-manager' ),
+					'editing'       => __( 'Editing: %s', 'wp-file-manager' ),
 				),
 			)
 		);
@@ -96,7 +94,7 @@ class WPFM_Admin {
 
 	public function render_page() {
 		if ( ! current_user_can( self::CAPABILITY ) ) {
-			wp_die( esc_html__( 'You do not have permission to access this page.', 'inaya-file-manager' ) );
+			wp_die( esc_html__( 'You do not have permission to access this page.', 'wp-file-manager' ) );
 		}
 		include WPFM_PLUGIN_DIR . 'admin/partials/wpfm-admin-page.php';
 	}
