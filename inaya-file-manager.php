b2a232cc044837ecb4a2602b6571f00e8ca96c9f
InayaTechLabs <InayaTechLabs@gmail.com>
2026-07-10 11:17:15 +0700

    Update inaya-file-manager.php

--- inaya-file-manager.php
+++ inaya-file-manager.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * Plugin Name:       Inaya File Manager
+ * Plugin Name:       WP File Manager
  * Plugin URI:        https://github.com/InayaTechLabs/WP-Files
+	
  * Description:       A lightweight, secure file manager for WordPress. Browse, upload, download, rename, and delete files inside your WordPress installation from the admin dashboard.
+	
  * Version:           1.0.0
  * Requires at least: 5.6
  * Requires PHP:      7.4
@@ -10,11 +12,10 @@
  * Author URI:        https://github.com/InayaTechLabs
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       inaya-file-manager
+ * Text Domain:       wp-file-manager
  * Domain Path:       /languages
- * Update URI:        https://github.com/InayaTechLabs/WP-Files
  *
- * @package Inaya_File_Manager
+ * @package WP_File_Manager
  */
 
 // If this file is called directly, abort.
@@ -43,6 +44,7 @@ require_once WPFM_PLUGIN_DIR . 'admin/class-wpfm-admin.php';
  * Activation hook.
  */
 function wpfm_activate() {
+	// Reserve for future setup (options, capabilities, etc.).
 	if ( ! get_option( 'wpfm_settings' ) ) {
 		add_option(
 			'wpfm_settings',
@@ -60,14 +62,16 @@ register_activation_hook( __FILE__, 'wpfm_activate' );
 /**
  * Deactivation hook.
  */
-function wpfm_deactivate() {}
+function wpfm_deactivate() {
+	// Intentionally left blank – keep user data on deactivation.
+}
 register_deactivation_hook( __FILE__, 'wpfm_deactivate' );
 
 /**
  * Load plugin text domain.
  */
 function wpfm_load_textdomain() {
-	load_plugin_textdomain( 'inaya-file-manager', false, dirname( WPFM_PLUGIN_BASENAME ) . '/languages' );
+	load_plugin_textdomain( 'wp-file-manager', false, dirname( WPFM_PLUGIN_BASENAME ) . '/languages' );
 }
 add_action( 'plugins_loaded', 'wpfm_load_textdomain' );
 
