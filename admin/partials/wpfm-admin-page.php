90c182c26a860b5025efd58b200e65e1ddd52e30
InayaTechLabs <InayaTechLabs@gmail.com>
2026-07-10 10:15:29 +0700

    Create wpfm-admin-page.php

---
+++ admin/partials/wpfm-admin-page.php
@@ -0,0 +1,13 @@
+<div class="wpfm-editor-header">
+	<strong id="wpfm-editor-title"></strong>
+	<span class="wpfm-editor-actions">
+		<button type="button" class="button" id="wpfm-editor-undo" title="<?php esc_attr_e( 'Undo', 'inaya-file-manager' ); ?>">
+			<span class="dashicons dashicons-undo"></span>
+		</button>
+		<button type="button" class="button" id="wpfm-editor-redo" title="<?php esc_attr_e( 'Redo', 'inaya-file-manager' ); ?>">
+			<span class="dashicons dashicons-redo"></span>
+		</button>
+		<button type="button" class="button button-primary" id="wpfm-editor-save"><?php esc_html_e( 'Save', 'inaya-file-manager' ); ?></button>
+		<button type="button" class="button" id="wpfm-editor-cancel"><?php esc_html_e( 'Cancel', 'inaya-file-manager' ); ?></button>
+	</span>
+</div>
